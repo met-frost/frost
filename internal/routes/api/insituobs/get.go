30d75e31537232652fc7e2b614b2605f68610659
Jo Asplin <jo.asplin@met.no>
2026-06-19 12:19:59 +0200

    Replaced back-end with backend

--- internal/routes/api/insituobs/get.go
+++ internal/routes/api/insituobs/get.go
@@ -164,7 +164,7 @@ func readItems(
 		return (*ts0).ObsFilter(t, body, reqInfo)
 	}
 
-	// call back-end function
+	// call backend function
 	itemCount, statusCode, err := sbe.ReadMultiTS(
 		tstype, tsSeq, tsHdrs, t1, t2, obsBodyModify, obsFilter, latestLimit, itemLimit,
 		&obs, reqInfo, context.Background())
